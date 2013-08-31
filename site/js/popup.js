63401475ad024ba777b3654a868c7917a9d1b0b8
user <user@contus-26le>
2013-08-31 11:11:08 +0530

    Commit for Contus HD Video Share version 3.1 for Joomla with channel

--- site/js/popup.js
+++ site/js/popup.js
@@ -1,4 +1,4 @@
-    <!--
+   
 var popUpWin=0;
 function popUpWindow(URLStr, left, top, width, height)
 {
@@ -7,4 +7,4 @@ function popUpWindow(URLStr, left, top, width, height)
     if(!popUpWin.closed) popUpWin.close();
   }
   popUpWin = open(URLStr, 'popUpWin', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=yes, width='+width+', height='+height+', left='+left+', top='+top+', screenX='+left+', screenY='+top+'');
-}//-->
+}
